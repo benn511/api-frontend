81e5e527ae07411f6bc4b61c94f9e1cd2df06024
benn511 <bgomez190@gmail.com>
2020-12-20 19:44:16 -0600

    adding new branch to test lifting state

---
+++ src/components/Events.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Axios from "axios";
+
+//Lifting state up to Event since no need to refresh page on new db entry
+class Events extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Events;
